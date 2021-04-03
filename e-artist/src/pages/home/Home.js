a94f14dab0a7220f15e97fe7fa9ac398fa6bf722
reveurguy <reveur.guy99@gmail.com>
2021-04-03 08:48:31 +0000

    changes by priyanshu

--- e-artist/src/pages/home/Home.js
+++ e-artist/src/pages/home/Home.js
@@ -8,17 +8,60 @@ function Home() {
       {/*Banner*/}
       <div className='home_banner'>
         <div className='home_bannerInfo'>
-          <h1>Art is beautiful, Art is life,<br />Buy Arts</h1>
+          <h1>Art is beautiful, Art is life<br />Buy Arts</h1>
           <h2>Browse our collection to find the masterpiece that you desire or <br />Join us today to start selling</h2>
-          <div className='home_bannerButtons'>
-            <button>Explore Artworks</button>
-            <button>Start Selling</button>
+          <div className='group-84'>
+            <button class="button1">Explore Artworks</button>
+            <button class="button2">Start Selling</button>
           </div>
         </div>
       </div>
 
       {/*Search*/}
+		  <div class="search">
+		  	<div class="rectangle35 ">
+				<div class="group87">
+					<div class="group86">
+				  <div class="rectangle36">
+					  
+				  </div>
+				</div>
+					<div class="group85">
+				  <div class="rectangle37">
+					  
+				  </div>
+				</div>
+					<div class="">
+						<button class="search-button"></button>
+					</div>
+					
+				</div>
+			  
+			</div>
+		  </div>
       {/*Featured Artworks*/}
+		  <div class="featured-artworks">
+			  <h1>Featured Artworks</h1>
+			  <button class="button3"></button>
+			  <div class="frame-97">
+				  <div class="group88">
+					  <div class="rectangle38">
+					  </div>
+				  </div>
+				  <div class="group89">
+					  <div class="rectangle39">
+					  </div>
+				  </div>
+				  <div class="group90">
+					  <div class="rectangle40">
+					  </div>
+				  </div>
+				  <div class="group91">
+					  <div class="rectangle41">
+					  </div>
+				  </div>
+			  </div>
+		  </div>
       {/*Top Artists*/}
       {/*Categories*/}
     </div>
