f5b887e9db4d75154df3a012669d959de6bf3cc9
jerome2l <55860345+J3r0m3L@users.noreply.github.com>
2021-04-03 17:16:39 -0700

    Linked up Navbar

--- e-artist/src/components/Navbar.js
+++ e-artist/src/components/Navbar.js
@@ -7,10 +7,9 @@ function Navbar() {
     <div className='navbar'>
       <Link to= "/" style={{ textDecoration: "none" }}><h1>Artisty</h1></Link>
       <div className='navbar_options'>
-        <Link to= "/" style={{ textDecoration: "none" }}><h2>Home</h2></Link>
-        <h2>Gallery</h2>
+        <Link to= "/landing" style={{ textDecoration: "none" }}><h2>Gallery</h2></Link>
         <Link to= "/leaderboard" style={{ textDecoration: "none" }}><h2>Artists</h2></Link>
-        <Link to= "/signup" style={{ textDecoration: "none" }}><h2>Login</h2></Link>
+        <Link to= "/upload" style={{ textDecoration: "none" }}><h2>Upload</h2></Link>
         <Link to= "/signup" style={{ textDecoration: "none" }}><button>Sign Up</button></Link>
       </div>
     </div>
